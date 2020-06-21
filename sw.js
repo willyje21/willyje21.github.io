52af2f6770d6b348698e448688577e6bddbb0308
willyje21 <66862542+willyje21@users.noreply.github.com>
2020-06-21 18:33:30 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -8,20 +8,8 @@ self.addEventListener('install', function(event) {
     .then(function(cache) {
       console.log('[Service Worker] Precaching App Shell');
       return cache.addAll([
- '/index.html',
- '/manifest.json',
- '/script/index.js',
- '/script/jquery-3.3.1.min.js',
- '/script/materialize.min.js',
- '/css/boostrap.min.css',
- '/css/materialize.min.css',
- '/images/icon/icon-48x48.png',
- '/images/icon/icon-96x96.png',
- '/images/icon/icon-144x144.png',
- '/images/icon/icon-192x192.png',
- '/images/icon/icon-256x256.png',
- '/images/icon/icon-384x384.png',
- '/images/icon/icon-512x512.png'
+        '/index.html',
+        '/offline.html'
       ])
     })
   )
