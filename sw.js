cd509f1c2e5de5603c02201496dece6ee650f4fa
willyje21 <66862542+willyje21@users.noreply.github.com>
2020-06-21 18:45:42 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,52 +1,67 @@
-var CACHE_STATIC_NAME = 'static-v7';
+const STATIC_ASSETS = [ 
+  './',
+ './index.html',
+ './manifest.json',
+ './script/index.js',
+ './script/jquery-3.3.1.min.js',
+ './script/materialize.min.js',
+ './css/boostrap.min.css',
+ './css/materialize.min.css',
+ './images/icon/icon-48x48.png',
+ './images/icon/icon-96x96.png',
+ './images/icon/icon-144x144.png',
+ './images/icon/icon-192x192.png',
+ './images/icon/icon-256x256.png',
+ './images/icon/icon-384x384.png',
+ './images/icon/icon-512x512.png'
+];
 
-self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
-  self.skipWaiting(); //PENTING bila ada versi baru!!
-  event.waitUntil(
-    caches.open(CACHE_STATIC_NAME)
-    .then(function(cache) {
-      console.log('[Service Worker] Precaching App Shell');
-      return cache.addAll([
-        './index.html'
-      ])
-      console.log('add sukses')
-    })
-  )
+const STATIC_CACHE_NAME = 'cache-static-v5';
+
+self.addEventListener('install', async evt => {
+    self.skipWaiting(); //PENTING bila ada versi baru!!
+  const cache = await caches.open(STATIC_CACHE_NAME);
+  cache.addAll(STATIC_ASSETS);
 });
 
+self.addEventListener('fetch', evt => {
+ const req = evt.request;
+ const url = new URL(req.url);
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
-  event.waitUntil(
-    caches.keys()
-      .then(function(keyList) {
-        return Promise.all(keyList.map(function(key) {
-          if (key !== CACHE_STATIC_NAME) {
-            console.log('[Service Worker] Removing old cache.', key);
-            return caches.delete(key);
-          }
-        }));
-      })
-  );
-  return self.clients.claim();
+ if (url.origin == location.origin) {
+  evt.respondWith(cacheFirst(req));
+ } else {
+  evt.respondWith(networkFirst(req));
+ }
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((resp) => {
-      return resp || fetch(event.request).then((response) => {
-        let responseClone = response.clone();
-        caches.open(CACHE_STATIC_NAME).then((cache) => {
-          cache.put(event.request, responseClone);
-        });
+async function cacheFirst(req) {
+ const cachedResponse = await caches.match(req);
+ return cachedResponse || fetch(req);
+}
 
-        return response;
-      });
-    }).catch(() => {
-      //console.log('ini belum ada inet');
-      //window.alert("sometext");
-      return caches.match('/index.html');
-    })
-  );
+async function networkFirst(req) {
+ const cache = await caches.open(STATIC_CACHE_NAME);
+ try {
+        // try go to network and fetch data 
+  const res = await fetch(req);
+  cache.put(req, res.clone());
+  return res;
+ } catch(error) {
+        // look something on cache. 
+  return await cache.match(req);
+ }
+}
+
+self.addEventListener('activate', (evt) => {
+ evt.waitUntil( 
+     caches.keys().then((keyList) => { 
+         return Promise.all(keyList.map((key) => { 
+             if (key !== STATIC_CACHE_NAME) { 
+                 console.log('[ServiceWorker] Removing old cache', key); 
+                 return caches.delete(key); 
+             } 
+         }));
+     })
+ ); 
 });
