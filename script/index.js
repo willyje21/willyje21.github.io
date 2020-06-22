bdac8482ad321edf2f90eba4eb618d51a3099363
willyje21 <66862542+willyje21@users.noreply.github.com>
2020-06-22 14:35:38 +0700

    Update index.js

--- script/index.js
+++ script/index.js
@@ -22,7 +22,7 @@ fetch(urlTrending,{
 
 //              document.getElementById("newsResults").innerHTML += "<div class='col-md-6'><div class='row'><div class='card-content'><span class='card-title activator'></span><h6 class='truncate'>Title: <a href="+d.articles[i].link+" title="+d.articles[i].title+">"+d.articles[i].title+"</a></h6><p><b>Author</b>:" +d.articles[i].author+" </p><p><b>News source</b>:"+d.articles[i].topic+" </p><p><b>Published</b>:" +d.articles[i].published_date+ "</p></div><div class='card-reveal'><span class='card-title'></span></div><div class='card-action'><a href="+d.articles[i].link+" target='_blank' class='btn'>Read More</a></div></div></div></div>";
 	    
-	    	document.getElementById("newsResults").innerHTML += "<div class='col-md-4 mt-3 text-center'><div class='card'><div class='card-header'><h5>" + d.articles[i].title + "</h5></div><div class='card-body'><h5>"+ "Author : "  + d.articles[i].author+"</h5><h5>"+d.articles[i].topic+"</h5><h5>"+d.articles[i].published_date+"</h5><div class='card-action'><a href="+d.articles[i].link+" target='_blank' rel='noopener' class='btn'>Read More</a></div></div></div></div>";
+	    	document.getElementById("newsResults").innerHTML += "<div class='col-md-4 mt-3 text-center'><div class='card'><div class='card-header'><h5>" + d.articles[i].title + "</h5></div><div class='card-body'><h5>"+  d.articles[i].author+"</h5><h5>"+d.articles[i].topic+"</h5><h5>"+d.articles[i].published_date+"</h5><div class='card-action'><a href="+d.articles[i].link+" target='_blank' rel='noopener' class='btn'>Read More</a></div></div></div></div>";
     }
 })
-
+ddx
