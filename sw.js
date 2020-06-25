d12f0b85bb6896a483e76981eb0395cb54543dbb
willyje21 <66862542+willyje21@users.noreply.github.com>
2020-06-25 15:11:08 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -66,3 +66,22 @@ self.addEventListener('activate', (evt) => {
      })
  ); 
 });
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        let responseClone = response.clone();
+        caches.open(CACHE_STATIC_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
+        });
+
+        return response;
+      });
+    }).catch(() => {
+      //console.log('ini belum ada inet');
+      //window.alert("sometext");
+      return caches.match('/offline.html');
+    })
+  );
+});
